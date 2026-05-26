97cc904d2035f236520c4a37f5cb4db3b27c0334
mansour <mansour.lolo@atlanticomedio.es>
2026-05-26 12:59:28 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/biblioteca',
   trailingSlash: true,
+  allowedDevOrigins: ['192.168.0.236', 'localhost'],
   images: {
     remotePatterns: [
       {
