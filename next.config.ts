98fabc6a51a9357415dacb2232e4a9c8bbb020d8
mansour <mansour.lolo@atlanticomedio.es>
2026-05-27 11:10:43 +0100

    fix docker file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/biblioteca',
-  output: 'standalone',
   allowedDevOrigins: ['192.168.0.236', 'localhost'],
   images: {
     remotePatterns: [
