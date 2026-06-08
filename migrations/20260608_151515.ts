3eee745a37af4ce10f8639b078ce09b3b30757ad
mansour lo lo <mansour.lolo@atlanticomedio.es>
2026-06-08 18:05:57 +0100

    updated migrations for link entry

--- migrations/20260608_151515.ts
+++ migrations/20260608_151515.ts
@@ -10,6 +10,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "news_news_items" ALTER COLUMN "tag" SET NOT NULL;
-  ALTER TABLE "news_news_items" DROP COLUMN "link";
+  -- ALTER TABLE "news_news_items" DROP COLUMN "link";
   ALTER TABLE "news" DROP COLUMN "visible_cards";`)
 }
